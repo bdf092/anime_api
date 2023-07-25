ce1edf9594ffdd8e50b6e3954fe92168c75ee096
EndlessEndless1999 <115580045+EndlessEndless1999@users.noreply.github.com>
2023-07-25 17:04:28 +0100

    links form to JS

--- client/index.js
+++ client/index.js
@@ -5,7 +5,20 @@
 
 
 
-
+//Form Elements
 const username = document.getElementById('username');
 const anime = document.getElementById('title');
 const genre = document.getElementById('genre');
+const year = document.getElementById('year');
+const review = document.getElementById('review');
+
+//Buttons
+const submit = document.getElementById('submit');
+const viewBtn = document.getElementById('view');
+
+
+
+submit.addEventListener('click', (event) => {
+    event.preventDefault();
+    console.log(username.value, anime.value, genre.value, year.value, review.value);
+})
