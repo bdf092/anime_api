b7cd0beabfeb8693033bbc7107727a20c7788932
EndlessEndless1999 <115580045+EndlessEndless1999@users.noreply.github.com>
2023-07-25 17:09:22 +0100

    adds createAnime func

--- client/index.js
+++ client/index.js
@@ -22,3 +22,32 @@ submit.addEventListener('click', (event) => {
     event.preventDefault();
     console.log(username.value, anime.value, genre.value, year.value, review.value);
 })
+
+
+
+
+const createAnime = (anime) => {
+    const options = {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json"
+        },
+        body: JSON.stringify({
+            name: anime.name,
+            title: anime.title,
+            genre: anime.genre,
+            year: anime.year,
+            review: anime.review
+        })
+    }
+    fetch(url, options)
+    .then(resp => resp.json())
+    .then(data => console.log(data))
+    .catch(err => console.log(err))
+}
+
+
+
+const viewAnime = () => {
+    return;
+}
